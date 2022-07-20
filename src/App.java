65260de9c56f4d3db11da03e89baa07021ea000d
Luis Felipe Andrade <luisfelipeandrade@gmail.com>
2022-07-19 21:06:54 -0300

    add parser class

--- src/App.java
+++ src/App.java
@@ -4,24 +4,48 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import util.ApiConsumer;
+import util.JsonParser;
 
 
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         System.out.println("Olá Cachoeiro");
 
        
         ApiConsumer apiConsumer = new ApiConsumer();
         String url = "https://imdb-api.com/en/API/Top250Movies/k_le3hl6wp";
+        url ="https://api.mocki.io/v2/549a5d8b";
         
         String json = apiConsumer.request(url);
 
-        System.out.println(json);
+        //System.out.println(json);
 
-        List<FilmeTO> listagemFilmes = new ArrayList();
+        //extração dos itens/parser
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(json);
+
+
+        //seleção/exibição dos intens
+        
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println("Filme: " + filme.get("title"));
+            System.out.println("Image: " + filme.get("image"));
+            System.out.println("Nota: "  + filme.get("imDbRating"));
+            System.out.println("");
+        }
+
+
+        //System.out.println(listaDeFilmes);
+        //System.out.println(listaDeFilmes.size());
+        //System.out.println(listaDeFilmes.get(1));
+        //System.out.println(listaDeFilmes.get(1).get("title"));
+
+        /*List<FilmeTO> listagemFilmes = new ArrayList();
         
         Filmes filmes = new Filmes() {
             
@@ -37,8 +61,10 @@ public class App {
         listagemFilmes.add(f3);
 
 
-        System.out.println(listagemFilmes.size());
+        System.out.println(listagemFilmes.size());*/
     }
+
+    
 
     
 }
